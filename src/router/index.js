e577c2c5d3b070b7215c97b53b72c6ee62a9e081
ThuThao527 <thuthao291204@gmail.com>
2024-10-10 18:41:44 +0700

    Cap nhat cuoi cung

--- src/router/index.js
+++ src/router/index.js
@@ -22,9 +22,9 @@ const routes = [
       props: true 
     },
     {
-      path: "/contacts/:id",
-      name: "contact.edit",
-      component: () => import("@/views/ContactEdit.vue"),
+      path: "/contacts/add",
+      name: "contact.add",
+      component: () => import("@/views/ContactAdd.vue"),
       props: true // Truyền các biến trong $route.params vào làm props
       },
 ];
